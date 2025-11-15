c5cb3957ea784d3532778730d66e81b2c5e73d1d
Abdur-Rehman15 <abdur.rehman8872@gmail.com>
2025-11-15 15:31:56 +0500

    backend structure created with test data

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from .config import Config
-
-db = SQLAlchemy()
+from .extensions import db, migrate
+from .routes import register_blueprints
+from app.models.user import User
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
+    migrate.init_app(app, db)
 
-    from .routes import main
-    app.register_blueprint(main)
+    register_blueprints(app)
 
     return app
